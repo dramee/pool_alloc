b7b9bb8f285b90cccc0ac1ca005061cc0a739209
dRamee <maksim.yolckin@yandex.com>
2022-12-27 19:29:19 +0700

    Init commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include
+
+#define MAX_SIZE 100 * 1024 * 1024
+
+typedef struct {
+    void* start;
+    void* end;
+} poolAlloc;
+
+void poolAllocInit(poolAlloc* poolAlloc) {
+    poolAlloc->start = malloc
+}
+
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
